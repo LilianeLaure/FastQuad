b83ec38aacac6bc3cdf7ee560fbf49487ed801f2
ERNEST Felix <3417212@pc4069.polytech.upmc.fr>
2016-11-23 18:16:33 +0100

    estimmateur sur les segments

--- GaussLegendre_carre.py
+++ GaussLegendre_carre.py
@@ -6,9 +6,29 @@ Created on Wed Nov  9 15:23:31 2016
 @author: 3417212
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+plt.close("all")
+
 """
 on se place sur [-1;1]x[-1;1]
 """
+def somme_de_GaussLegendre2D(N, alpha1, beta1, alpha2, beta2,func ):
+    x,y = np.polynomial.legendre.leggauss(N)
+    
+    #homothétie de [-1;1] vers [alpha;beta]
+    x1 = (beta1 - alpha1)/2 * x + (alpha1 + beta1)/2
+    x2 = (beta2 - alpha2)/2 * x + (alpha2 + beta2)/2
+    I_exp = (beta1-alpha1)*sum(y*func(x))/2.
+    return I_exp
+    
+def estimateur_erreur(N, f, alpha , beta ):
+    Sf = somme_de_GaussLegendre(N,alpha, beta, f)
+    #print "Sf = ", Sf, " pour n = ",N
+    Sf2 = somme_de_GaussLegendre(N, alpha, (alpha + beta)/2., f) + somme_de_GaussLegendre(N, (alpha + beta)/2., beta, f)
+    #print "Sf2 = ", Sf2, " pour n = ",N
+    return Sf - Sf2
+
 def erreur_de_GaussLegendre2D(N, func, SS):
     X,Y = np.polynomial.legendre.leggauss(N)
     I_exp = 0
@@ -45,18 +65,18 @@ valh = errh=errg = errf = np.array([])
 
 for n in N:
     #errf = np.append(errf, erreur_de_GaussLegendre2D(n, f, 8.0/3))
-    errh = np.append(valh, erreur_de_GaussLegendre2D(n, h,0.386))
+    errh = np.append(errh, erreur_de_GaussLegendre2D(n, h,0.86))
 
-
+"""
 fig4=plt.figure()
 plt.plot(np.log(N), np.log(errf), 'r')
 plt.xlabel('log(N)')
 plt.ylabel('log(errf)')
-fig1.suptitle('erreur de Gauss Legendre pour f(x,y)=x²+y²', fontsize=20)
-fig1.savefig('f2D.jpg')
-plt.show()
+fig4.suptitle('erreur de Gauss Legendre pour f(x,y)=x²+y²', fontsize=20)
+fig4.savefig('f2D.jpg')
+plt.show()"""
 
-fig3=plt.figure()
+fig5=plt.figure()
 plt.plot(np.log(N), np.log(errh), 'b')
 plt.xlabel('log(N)')
 plt.ylabel('log(errh)')
--- GaussLegendre_segment.py
+++ GaussLegendre_segment.py
@@ -3,16 +3,24 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+plt.close("all")
 
-def erreur_de_GaussLegendre(N, func, SS):
+def somme_de_GaussLegendre(N, alpha, beta, func ):
     x,y = np.polynomial.legendre.leggauss(N)
-    I_exp = sum(y*func(x))
-    print "N:"+str(N)
-    print "I_exp:"+str(I_exp)
-    print "I_th:"+str(SS)
-    print "abs(I_th - I_exp):"+str(abs(SS - I_exp))
-    print ""   
-    return abs(SS - I_exp)
+    
+    #homothétie de [-1;1] vers [alpha;beta]
+    x = (beta - alpha)/2 * x + (alpha + beta)/2
+    
+    I_exp = (beta-alpha)*sum(y*func(x))/2.
+    return I_exp
+    
+def estimateur_erreur(N, f, alpha , beta ):
+    Sf = somme_de_GaussLegendre(N,alpha, beta, f)
+    #print "Sf = ", Sf, " pour n = ",N
+    Sf2 = somme_de_GaussLegendre(N, alpha, (alpha + beta)/2., f) + somme_de_GaussLegendre(N, (alpha + beta)/2., beta, f)
+    #print "Sf2 = ", Sf2, " pour n = ",N
+    return Sf - Sf2
+
 
 def f(x):    
     return 4
@@ -27,14 +35,22 @@ def h(x):
 N = np.linspace(1, 100, 100)
 errh=errg = errf = np.array([])
 
+
+alpha=-1
+beta=1
 for n in N:
-    #errf = np.append(errf, erreur_de_GaussLegendre(n, f, 8))
-    #errg = np.append(errg, erreur_de_GaussLegendre(n, g, 2.0/51))
+    errf = np.append(errf, estimateur_erreur(n,f, alpha, beta))
+    errg = np.append(errg, estimateur_erreur(n,g, alpha, beta))
     if (mod(n,2) == 0):
-        errh = np.append(errh, erreur_de_GaussLegendre(n, h, 1))
+        errh = np.append(errh, estimateur_erreur(n,h, alpha, beta))
 	#print(h(n))
-   
+
 """
+print "erreur par rapport à f : ", errf
+print "erreur par rapport à g : ", errg
+print "erreur par rapport à h : ", errh
+
+
 fig1=plt.figure()
 plt.plot(np.log(N), np.log(errg), 'r')
 plt.xlabel('log(N)')
@@ -42,6 +58,7 @@ plt.ylabel('log(errg)')
 fig1.suptitle('erreur de Gauss Legendre pour g(x)=x^50', fontsize=20)
 fig1.savefig('g.jpg')
 plt.show()
+
 fig2=plt.figure()
 plt.plot(np.log(N), np.log(errf), 'b')
 plt.xlabel('log(N)')
@@ -49,12 +66,13 @@ plt.ylabel('log(errf)')
 fig2.suptitle('erreur de Gauss Legendre pour f(x)=4', fontsize=20)
 fig2.savefig('f.jpg')
 plt.show()
-""" 
+"""
 
 
 fig3=plt.figure()
 plt.plot(np.log(N[0:-1:2]), np.log(errh), 'b')
 plt.xlabel('log(N)')
 plt.ylabel('log(errh)')
+fig3.suptitle('erreur de Gauss Legendre pour h(x)=|x|', fontsize=20)
 plt.show()
 #...
