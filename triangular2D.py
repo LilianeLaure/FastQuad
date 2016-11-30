7607c3fe8a0fce0d14d6fa3b55b309f6e1aa2ee5
AHNE Adrian <3503833@pc4022.polytech.upmc.fr>
2016-11-30 16:55:01 +0100

    Triangular regular 2D montecarlo

--- triangular2D.py
+++ triangular2D.py
@@ -20,50 +20,47 @@ def monteCarlo_Regul_Tria(A,B,C,N):
 
 def monteCarlo_Tria(P0,P1,N):
     f_moy=sum(P0**2+P1**2)/N
-    f_moy2=sum((P0**2+P1**2)**2)/N
-    err=np.sqrt(np.abs((f_moy2-f_moy**2)/N))
-    
-    #err = np.sqrt(np.abs((1/(N-1)* sum((P[0]**2+P[1]**2) - f_moy )**2)/N))
-    
+    print "f_moy", f_moy
+    f_moy2=sum((P0**2+P1**2)**2)/2
+    #err=np.sqrt(np.abs((f_moy2-f_moy**2)/N))
+    Var=np.abs((1/N)*sum((P0**2+P1**2)**2)-f_moy**2)
+    #print "valeur",sum (P0**2+P1**2-f_moy)**2
+    err = np.sqrt(Var/N)*1.96
+    print "var", Var
     print "f_err", err   
     return err
 
 
 #figure()
-A=np.array([4,1])
-B=np.array([1,0])
-C=np.array([0,3])
-
+A=np.array([1,1])
+B=np.array([4,1])
+C=np.array([5,3])
 Point=monteCarlo_Regul_Tria(A,B,C,N)
 print "point:",Point
 print "point0",Point[0][0]
 print "point1",Point[1][0]
 plt.figure
+'''
 plt.scatter(A[0], A[1])
 plt.scatter(B[0], B[1])
 plt.scatter(C[0], C[1])
 plt.scatter(Point[0],Point[1])
 #plt.show()
-
+'''
+N=size(Point[0])
+print "N",N
+monteCarlo_Tria(Point[0],Point[1],N)
 
 err=np.array([])
 figure()
-for n in range(1,N):
+for n in range(100,N):
+    Point=monteCarlo_Regul_Tria(A,B,C,n)
     err=np.append(err,monteCarlo_Tria(Point[0],Point[1],n))
-plt.plot(log(range(1,N)), log(err))
-err2=log(err)
-x=log(range(1,N))
-print size(x)
-print size(err)
-z=x[1]
-y=err2[1]
-print z,y
-a=x[500]
-b=err2[500]
-print a,b
-pente=(b-y)/(a-z)
-print pente
-plt.title("Case 2D triangular regular polynom $x^{2}+y^{2}$")
+plt.plot(log(range(100,N)),log(err),"b")
+plt.plot(log(range(100,N)), -0.5*log(range(100,N))+3.37, "r")
+
+
+plt.title("Case 2D triangular regular polynom $x^{2}+y^{2}$|| pente = -0.5")
 plt.xlabel("log(N)")
 plt.ylabel("log(err)")
 plt.show()
