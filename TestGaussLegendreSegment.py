f2694d1c2fef6a353a135b114f40d19b977b3b35
AHNE Adrian <3503833@pc4069.polytech.upmc.fr>
2017-02-15 17:47:35 +0100

    images 2 segment singular more eps

--- TestGaussLegendreSegment.py
+++ TestGaussLegendreSegment.py
@@ -20,12 +20,12 @@ on se place sur [-1;1]x[-1;1]
 
 # regular function
 def f(x,y):
-	return exp(norm(x) + norm(y))#norm(x)**2 + norm(y)**2	#sin(norm(x) + norm(y))
+	return exp(x[0]**2 + x[1]**2 + y[0]**2 + y[1]**2) #return sum(exp(x**2 + y**2))  #norm(x)**2 + norm(y)**2	#sin(norm(x) + norm(y))
 	#return norm(x**2) + norm(y**2)
     
 # singular function
 def h(x,y):   
-    return 1/(norm(norm(x) - norm(y)))
+    return 1/(norm(x - y))
 				
 				
 
@@ -60,7 +60,13 @@ def calculateInt(int1, int2, order, t, w):
 	for i, gi in enumerate(gamma.T):
 		for j, sj in enumerate(sigma.T):
 			#print "i =", i, "j =", j, "gi =", gi, "sj =",sj
-			I = I + w[i]*w[j]*f(gi, sj)
+			
+			# regular function
+			#I = I + w[i]*w[j]*f(gi, sj)
+			
+			# singular function
+			I = I + w[i]*w[j]*h(gi, sj)
+
 			
 
 	I = length_Segment(a1, b1)*length_Segment(a2, b2)*I
@@ -71,19 +77,19 @@ def calculateInt(int1, int2, order, t, w):
 
 
 # composite Gauss - Legendre
-def calculateCompositeInt(int1, int2, N, order, t, w):
+def calculateCompositeInt(int1, int2, Nb_Int, order, t, w):
 	
 	# intervalls
 	a1, b1 = int1
 	a2, b2 = int2	
 	
-	numberIntervalls = N/order
+	#numberIntervalls = N/order
 	#print "numberIntervalls: ", numberIntervalls
 	I = 0
-	for i in range(0, numberIntervalls):
-		for j in range(0, numberIntervalls):
-			newInt1 = [np.array(a1 + i*(b1-a1)/numberIntervalls), np.array(a1 + (i+1)*(b1-a1)/numberIntervalls)]
-			newInt2 = [np.array(a2 + j*(b2-a2)/numberIntervalls), np.array(a2 + (j+1)*(b2-a2)/numberIntervalls)]
+	for i in range(0, Nb_Int):
+		for j in range(0, Nb_Int):
+			newInt1 = [np.array(a1 + i*(b1-a1)/Nb_Int), np.array(a1 + (i+1)*(b1-a1)/Nb_Int)]
+			newInt2 = [np.array(a2 + j*(b2-a2)/Nb_Int), np.array(a2 + (j+1)*(b2-a2)/Nb_Int)]
 			#print "i =", i, "j =", j, "a1 =", a1, "b1 =", b1, "a2 =", a2, "b2 =", b2		
 			#print "new [a1, b1] =", newInt1
 			#print "new [a2, b2] =", newInt2
@@ -96,16 +102,16 @@ def calculateCompositeInt(int1, int2, N, order, t, w):
 
 	
 	
-def calculateError(int1, int2, N, order, t, w):
+def calculateError(int1, int2, Nb_Int, order, t, w):
 	
 	# intervalls
 	a1, b1 = int1
 	a2, b2 = int2
 
-	numberIntervalls = N/order
+	#numberIntervalls = N/order
 	
 	
-	I = calculateCompositeInt(int1, int2, N, order, t, w)
+	I = calculateCompositeInt(int1, int2, Nb_Int, order, t, w)
 	
 	# subdivise both segments to calculate error
 	
@@ -118,19 +124,19 @@ def calculateError(int1, int2, N, order, t, w):
 	int22 = [(int2[0]+int2[1])/2, int2[1]]
 	
 	# calculate sum of subIntervalls
-	I1 = calculateCompositeInt(int11, int21, N, order, t, w)
+	I1 = calculateCompositeInt(int11, int21, Nb_Int, order, t, w)
 	#print "I1 =", I1
-	I2 = calculateCompositeInt(int11, int22, N, order, t, w) 
+	I2 = calculateCompositeInt(int11, int22, Nb_Int, order, t, w) 
 	#print "I2 =", I2
-	I3 = calculateCompositeInt(int12, int21, N, order, t, w)
+	I3 = calculateCompositeInt(int12, int21, Nb_Int, order, t, w)
 	#print "I3 =", I3
-	I4 = calculateCompositeInt(int12, int22, N, order, t, w)
+	I4 = calculateCompositeInt(int12, int22, Nb_Int, order, t, w)
 	#print "I4 =", I4
 	I_sub = I1 + I2 + I3 + I4
 
 	err = abs(I - I_sub)/abs(I)
-	print "I =", I , "I_sub =", I_sub
-	print "error: ", err
+	#print "I =", I , "I_sub =", I_sub
+	#print "error: ", err
 
 	return err
 	
@@ -143,41 +149,61 @@ def calculateError(int1, int2, N, order, t, w):
 ###############################################################################
 ###############################################################################
 
-epsilon = 0.5
+#epsilon = 0.5
 
-a1 = np.array([-epsilon, -1.0])
-b1 = np.array([-epsilon, +1.0])
-
-a2 = np.array([epsilon, -1.0])
-b2 = np.array([epsilon, +1.0])
-
-
-
-int1=[a1, b1]
-int2=[a2, b2]
-
-
+#a1 = np.array([-epsilon, -1.0])
+#b1 = np.array([-epsilon, +1.0])
+#
+#a2 = np.array([epsilon, -1.0])
+#b2 = np.array([epsilon, +1.0])
+#
+#
+#
+#int1=[a1, b1]
+#int2=[a2, b2]
 
 
-order = 5 # ordre for the method gaussLegendre
-N = 300 # number of points
+
+
+order = 4 # ordre for the method gaussLegendre
+
+numberSubIntervalls = 50
+#N = 300 # number of points
 
 # test if we can calculate a straight number for the interval like 200, 242 and NOT 233,343
-if N%order != 0:
-	print "ERROR: Give N and order s.t. N/order is an integer!"
-	exit()
+#if N%order != 0:
+#	print "ERROR: Give N and order s.t. N/order is an integer!"
+#	exit()
 
-rangeN = range(order, N, order)
+#rangeN = range(order, N, order)
+rangeN = range(1, numberSubIntervalls)
+
 
 
 t,w = np.polynomial.legendre.leggauss(order) # t in [-1,1], w weight, ordre 50
 
-errSing = np.array([])
-for n in rangeN:
-	errSing = np.append( errSing, calculateError(int1, int2, n, order, t, w))
+plt.figure()
+for epsilon in [1, 2, 3]:
+	print epsilon
+	a1 = np.array([-epsilon, -1.0])
+	b1 = np.array([-epsilon, +1.0])
+
+	a2 = np.array([epsilon, -1.0])
+	b2 = np.array([epsilon, +1.0])
+
+	int1=[a1, b1]
+	int2=[a2, b2]
+	
+	errSing = np.array([])
+	for n in rangeN:
+		errSing = np.append( errSing, calculateError(int1, int2, n, order, t, w))
 	
 #print "coucou", calculateInt(int1, int2, order, t, w)	
-	
-plot(np.log(rangeN), np.log(errSing) )
-plt.title("regular function, order 5, N = 500")
+	print(np.log(errSing))
+	plt.plot(np.log(rangeN), np.log(errSing), label=epsilon )
+	plt.hold(True)
+	plt.legend()
+plt.title("singular function for different eps")
+plt.yaxis("error")
+plt.xaxis("ln(number sub intervalls)")
 
